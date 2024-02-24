92810d0e4de0c4a1fd386aba7bd8b0a0c8bb8b65
Tanumoy <tanumoy.academy@gmail.com>
2024-02-24 11:10:25 +0530

    Initial Push.

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!" + sum(1, 2));
+    }
+
+    public static int sum(int a, int b) {
+        return a + b;
+    }
+}
