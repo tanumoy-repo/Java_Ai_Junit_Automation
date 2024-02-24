9bdf82863066a0cf11b0f0f062898d2824a6cf82
Tanumoy <tanumoy.academy@gmail.com>
2024-02-24 13:13:45 +0530

    Initial Push.

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,5 +1,8 @@
 package org.example;
 
+/**
+ * Install Codeium  plugins in IntelliJ IDEA and automatically an icon will come. Click there and on the right hand panel you will see the tests.
+ */
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!" + sum(1, 2));
